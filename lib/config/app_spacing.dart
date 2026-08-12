373f2e751e6d7b4f72e7adbb605257bffc0d0a7b
Damilare Hamed <thinkdare.dev@gmail.com>
2026-08-12 09:04:33 +0100

    feat: add AppSpacing/AppRadius design tokens

---
+++ lib/config/app_spacing.dart
@@ -0,0 +1,16 @@
+// lib/config/app_spacing.dart
+class AppSpacing {
+  AppSpacing._();
+  static const double xs = 4;
+  static const double sm = 8;
+  static const double md = 12;
+  static const double lg = 16;
+  static const double xl = 24;
+}
+
+class AppRadius {
+  AppRadius._();
+  static const double control = 12;
+  static const double card = 16;
+  static const double pill = 999;
+}
